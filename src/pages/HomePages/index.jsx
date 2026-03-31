a9443a2c5f0d89d8545ecda24332b9f0921f76f8
artem/s <a.skorikov.fortech@gmail.com>
2026-03-31 20:05:36 +0300

    feat: display task

--- src/pages/HomePages/index.jsx
+++ src/pages/HomePages/index.jsx
@@ -1,64 +1,13 @@
 import { Component } from "react";
 import TodoList from "../../components/TodoList";
-import AddForm from "../../components/AddForm";
 import Footer from "../../components/Footer";
 import initialExpenses from "../../constants";
 import "./styles.scss";
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tasks: [],
-      newTaskText: "",
-      error: "",
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.validateForm = this.validateForm.bind(this);
-    this.addTask = this.addTask.bind(this);
-  }
-
-  handleInputChange(e) {
-    this.setState({
-      newTaskText: e.target.value,
-      error: "",
-    });
-  }
-
-  validateForm() {
-    const { newTaskText } = this.state;
-
-    if (!newTaskText.trim()) {
-      this.setState({
-        error: "Поле не должно быть пустым",
-      });
-      return;
-    }
-
-    this.addTask();
-  }
-
-  addTask() {
-    const { newTaskText, tasks } = this.state;
-
-    const newTask = {
-      id: Date.now(),
-      text: newTaskText.trim(),
-      completed: false,
-    };
-
-    const updatedTasks = [newTask, ...tasks];
-
-    this.setState({
-      tasks: updatedTasks,
-      newTaskText: "",
-      error: "",
-    });
-
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-  }
+  state = {
+    tasks: [],
+  };
 
   componentDidMount() {
     const savedTasks = localStorage.getItem("tasks");
@@ -68,11 +17,7 @@ class HomePage extends Component {
   }
 
   render() {
-    const {
-      tasks,
-      newTaskText,
-      error
-    } = this.state;
+    const { tasks } = this.state;
 
     const tasksCount = tasks.length;
 
@@ -80,18 +25,8 @@ class HomePage extends Component {
       <div className="home-page">
         <h1 className="home-page__title">To do</h1>
         <div className="home-page__card">
-          <AddForm
-            newTaskText={newTaskText}
-            error={error}
-            handleInputChange={this.handleInputChange}
-            validateForm={this.validateForm}
-          />
-          <TodoList
-            tasks={tasks}
-          />
-          <Footer
-            tasksCount={tasksCount}
-          />
+          <TodoList tasks={tasks} />
+          <Footer tasksCount={tasksCount} />
         </div>
       </div>
     );
